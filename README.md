abd2772db56274e2e7e6d98d3e5d99d22750826f
peri <periarta2004@gmail.com>
2025-07-10 11:02:16 +0800

    update data

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Menjadi Google Cloud Engineer
 
 ## Courses
-``` bash
-https://www.dicoding.com/academies/133/corridor
-```
 
-## Donlowad respository 
-``` bash
+[https://www.dicoding.com/academies/133/corridor](https://www.dicoding.com/academies/342/corridor)
+
+## Donlowad respository
+
+```bash
 https://github.com/periartaa/notes-app-back-end.git
 ```
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,36 @@
-// const { addNoteHandler } = require("./handler");
-const { addNoteHandler, getAllNotesHandler } = require("./handler");
+const {
+  siveBookHandler,
+  displayAllBooksHandler,
+  detailBookHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler,
+} = require("./handler");
 
 const routes = [
   {
     method: "POST",
-    path: "/notes",
-    handler: addNoteHandler,
+    path: "/books",
+    handler: siveBookHandler,
   },
   {
     method: "GET",
-    path: "/notes",
-    handler: getAllNotesHandler,
+    path: "/books",
+    handler: displayAllBooksHandler,
+  },
+  {
+    method: "GET",
+    path: "/books/{id}",
+    handler: detailBookHandler,
+  },
+  {
+    method: "PUT",
+    path: "/books/{id}",
+    handler: editBookByIdHandler,
+  },
+  {
+    method: "DELETE",
+    path: "/books/{id}",
+    handler: deleteBookByIdHandler,
   },
 ];
 
